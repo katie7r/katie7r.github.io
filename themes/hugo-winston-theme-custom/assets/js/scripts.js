6ec71a131efe9c2d075a9edf9c598fa3b8e53d6f
Katie Linero <katie.k7r@gmail.com>
2024-01-12 21:06:12 -0600

    Add dark-mode toggling

--- themes/hugo-winston-theme-custom/assets/js/scripts.js
+++ themes/hugo-winston-theme-custom/assets/js/scripts.js
@@ -10,3 +10,27 @@ if (menuTrigger !== null) {
     body.classList.toggle('lock-scroll');
   });
 }
+
+// Set/toggle light/dark mode
+
+function setThemeMode(themeMode) {
+  if (themeMode === 'dark') {
+    body.classList.add('dark-mode');
+  } else {
+    body.classList.remove('dark-mode');
+  }
+}
+
+document.getElementById('toggle-theme-mode').addEventListener('click', function(e) {
+  e.preventDefault();
+
+  var themeMode = body.classList.contains('dark-mode') ? 'light' : 'dark';
+
+  localStorage.setItem('theme-mode', themeMode);
+  setThemeMode(themeMode);
+}, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var themeMode = localStorage.getItem('theme-mode') || 'light';
+  setThemeMode(themeMode);
+});
